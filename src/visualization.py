8fb06cc55445f9a2f2863cbe2bbbe91ca9ff9573
alriss <alberto.rissone.ar@gmail.com>
2025-03-27 21:39:49 +0100

    refactor visualization.py and remove unused chart.png; update plot aesthetics and save figures as PDFs

--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-from matplotlib.patches import PathPatch
+from matplotlib.patches import PathPatch, Patch
 from matplotlib.path import Path
 import numpy as np
 
@@ -24,10 +24,6 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
     # Creazione del grafico
     fig, ax = plt.subplots()
     fig.set_size_inches(25 / 365 * len(df_pm10), 8)
-
-    for i,d in enumerate(df_pm10["Data rilevamento"].dt.day):
-        if d == 1 or i == len(df_pm10)-1:
-            ax.axvline(x=xx_all[i], color="#000", ls="--", lw=0.5)          # Linee vert. per il primo del mese e l'ultimo dell'anno
 
     ax.plot(xx, yy, color="#bbb", alpha=1, ls="--", zorder=0.9, lw = 0.75)  # Linea tratteggiata per i valori nulli  
     ax.plot(xx_all, yy_all, color="#999", alpha=1, zorder=1, lw = 0.75)     # Linea continua
@@ -62,9 +58,8 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
     )
 
     # Parametri grafici
-    h_hmp     = 15
     side_mrg  = 1
-    upper_mrg = 10
+    upper_mrg = 2
 
     ax.axis([
         min(xx) - side_mrg, 
@@ -75,7 +70,7 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
 
     # Linea orizz. per il limite di legge
     ax.axhline(y = 50, color = "firebrick", ls = "--", lw = 1) 
-    ax.text(x = (ax.get_xlim()[0] + ax.get_xlim()[1]) / 2,  
+    ax.text(x = ax.get_xlim()[0]+(ax.get_xlim()[1] - ax.get_xlim()[0]) / 1.5,  
             y = 50 ,  
             s = "Limite di legge: 50 µg/m³",  
             color = "firebrick", 
@@ -87,59 +82,59 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
     set_axes_color(ax, None)
     ax.grid(axis="y", alpha=0.25)
 
+    ax.axes.get_xaxis().set_ticks([])
     ticks = ax.get_yticks()[1:-1]                           # Esclude il primo e l'ultimo tick dell'asse y
     ax.yaxis.set_ticks(ticks)  
     ax.yaxis.set_ticklabels([f"{i} µg/m³" for i in ticks])  # Aggiunge l'unità di misura
-    ax.axes.get_xaxis().set_ticks([])
+    ax.axes.get_yaxis().set_ticks([])
+
+    for tick in ticks:
+        ax.axhline(y = tick, color = "grey", alpha=0.25, lw=.5, zorder=-1) 
+        ax.text(x = ax.get_xlim()[0]+(ax.get_xlim()[1] - ax.get_xlim()[0]) / 2.5,  
+            y = tick ,  
+            s = f"{tick} µg/m³",  
+            color = "black", 
+            alpha=0.45,
+            fontsize = 8, 
+            bbox = dict(facecolor = 'white', alpha = 1, edgecolor = 'white'),
+            ha = "center", 
+            va = "center")
 
     m_lab = [
-        "GENNAIO",
-        "FEBBRAIO",
-        "MARZO",
-        "APRILE",
-        "MAGGIO",
-        "GIUGNO",
-        "LUGLIO",
-        "AGOSTO",
-        "SETTEMBRE",
-        "OTTOBRE",
-        "NOVEMBRE",
-        "DICEMBRE",
+        "Gennaio",
+        "Febbraio",
+        "Marzo",
+        "Aprile",
+        "Maggio",
+        "Giugno",
+        "Luglio",
+        "Agosto",
+        "Settembre",
+        "Ottobre",
+        "Novembre",
+        "Dicembre",
     ]
 
     first_days = xx_all[df_pm10["Data rilevamento"].dt.day == 1]
 
     for i, f in enumerate(first_days): # Aggiunge le annotazioni in alto
-
-        val_mese = df_pm10["Valore"][ind_val][df_pm10["Data rilevamento"][ind_val].dt.month == (i % 12) + 1]
-        mx  = max(val_mese)
-        avg = sum(val_mese) / len(val_mese)
-
-        x_fact  = max(xx) - min(xx) + 2 * side_mrg 
-        y_fact  = (max(yy) + upper_mrg - (max(yy) + upper_mrg) / h_hmp,)
-        x       = (f - (min(xx) - side_mrg)) / x_fact
-        y       = 1
-        w       = float(31 / x_fact) if i < 11 else float(30 / x_fact)
-        h       = float(((max(yy) + upper_mrg) / h_hmp / 3 / y_fact).item())
-        
-        inset_ax = ax.inset_axes([x, y, w, h], xticks=[], xticklabels=[], yticks=[], yticklabels=[])
-        set_axes_color(inset_ax,'#666')
-
-        inset_ax.set_title(m_lab[i % 12])
-        inset_ax.set_facecolor(customColourMap(mx / (max(yy) + 5)))
-        inset_ax.set_xlabel(f"{round(avg, 2)} µg/m³")
-
-    ax_x = ax.get_xlim()
-    ax_y = ax.get_ylim()
-
-    ax.annotate("Media ", 
-                xy = (ax_x[0], ax_y[1]), 
-                xytext = (ax_x[0], ax_y[1] - (ax_y[1] - ax_y[0])*.008 ), 
-                ha = "right", 
-                va = "top", 
-                fontsize = 11)
-
-    # Aggiunge le annotazioni in basso (Pioggia)
+        ax.text(x = f+15,  
+            y = 2.5 ,  
+            s = m_lab[i],  
+            color = "black", 
+            alpha=0.45,
+            fontsize = 10, 
+            ha = "center", 
+            va = "top")
+        if i>0:
+            ax.text(x = f-.5,  
+                y = 2.5 ,  
+                s = '|',  
+                color = "black", 
+                alpha=0.45,
+                fontsize = 13, 
+                ha = "center", 
+                va = "top")
 
     xx_piogge = xx_all[df_piogge["Pioggia"] == 1]
     yy_piogge = np.ones(len(xx_piogge))
@@ -157,16 +152,6 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
     inset_ax.scatter(xx_piogge, yy_piogge, color="cornflowerblue", alpha=1, s=100, marker="|")
     inset_ax.invert_yaxis()
 
-    inset_ax.annotate("Pioggia  ", 
-                      xy = (inset_ax.get_xlim()[0], inset_ax.get_ylim()[0]), 
-                      xytext = (inset_ax.get_xlim()[0], inset_ax.get_ylim()[0] + (inset_ax.get_ylim()[1] - inset_ax.get_ylim()[0])*.80 ), 
-                      ha = "right", 
-                      va = "top", 
-                      fontsize = 12,
-                      color = '#496ca8')
-
-    # Aggiunge le annotazioni in basso (Pioggia)
-
     xx_temp = xx_all[df_temp["Temperatura minima"] < temp_thr]
     yy_temp = np.ones(len(xx_temp))
     
@@ -183,18 +168,14 @@ def plot_pm10(df_pm10, df_piogge, df_temp, temp_thr=6):
     inset_ax.scatter(xx_temp, yy_temp, color="plum", alpha=1, s=100, marker="|")
     inset_ax.invert_yaxis()
 
-    ax_x = inset_ax.get_xlim()
-    ax_y = inset_ax.get_ylim()
+    legend_elements = [Patch(facecolor='cornflowerblue',label='Pioggia'),
+                       Patch(facecolor='plum',label='T min < 6 °C')]
 
-    inset_ax.annotate(f'T min < {str(temp_thr)}°C  ', 
-                      xy = (ax_x[0], ax_y[0]), 
-                      xytext = (ax_x[0], ax_y[0] + (ax_y[1] - ax_y[0])*.98 ), 
-                      ha = "right", 
-                      va = "top", 
-                      fontsize = 12,
-                      color = '#ad61ac')
-
-    plt.show()
+    ax.legend(handles=legend_elements, ncols=2, frameon=False, loc='lower right', bbox_to_anchor=(1, -0.15))
+    
+    #plt.show()
+    plt.title('PM10 (Torino, 2024)', loc='center', fontdict={'fontsize': 24, 'fontweight': 'bold'})
+    plt.savefig('../pm10_torino.pdf', bbox_inches='tight')
 
 def plot_correlation(df, lags, ccf, pvals):
 
@@ -203,7 +184,7 @@ def plot_correlation(df, lags, ccf, pvals):
     fig.set_size_inches(8, 4+0.3*len(lags))
 
     # Grafico a barre
-    ax.barh(lags, width=abs(ccf)+ 0.01, color='grey', height=0.1)
+    ax.barh(lags, width=abs(ccf)+ 0.01, color='grey', height=0.05)
     
     # Rimuove gli assi destro e inferiore
     ax.spines['right'].set_visible(False)
@@ -238,4 +219,6 @@ def plot_correlation(df, lags, ccf, pvals):
     ax.set_title(f'Cross-Correlation {df.columns[0]} vs {df.columns[1]}')
     ax.axes.get_xaxis().set_ticks([])
     ax.invert_yaxis() 
-    plt.show()
+
+    #plt.show()
+    plt.savefig(f'../cross_correlation_chart_{df.columns[1]}.pdf', bbox_inches='tight')
